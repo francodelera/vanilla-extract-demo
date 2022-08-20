35c6d5d56dba257b534e14f60cac3910e20702e7
francodelera <39776660+francodelera@users.noreply.github.com>
2022-08-20 18:34:15 -0300

    Create global theme example with vars

---
+++ src/theme.css.ts
@@ -0,0 +1,33 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+export const vars = createGlobalTheme(":root", {
+  colors: {
+    blue: "#383182",
+    pink: "#7B47F3",
+    orange: "#F96E62",
+  },
+  space: {
+    extraExtraSmall: "0.06rem",
+    extraSmall: "0.25rem",
+    small: "8px",
+    medium: "16px",
+    large: "32px",
+    extraLarge: "64px",
+    extraExtraLarge: "128px",
+  },
+  fonts: {
+    fontSizes: {
+      small: "0.75rem",
+      medium: "1rem",
+      large: "2rem",
+      extraLarge: "3rem",
+      extraExtraLarge: "4rem",
+    },
+    fontFamily: {
+      Gilroy: "Gilroy",
+      GilroyLight: "Gilroy-Light",
+      GilroyMedium: "Gilroy-Regular",
+      GilroyBold: "Gilroy-Bold",
+    },
+  },
+});
