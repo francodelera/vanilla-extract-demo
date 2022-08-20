7a60b820350cc1613d3b4a6f21aff194ea38aa97
francodelera <39776660+francodelera@users.noreply.github.com>
2022-08-20 18:21:39 -0300

    Adds production mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,66 +1,71 @@
 import path from "path";
-import {Configuration} from "webpack";
+import { Configuration } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
-import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
-import {VanillaExtractPlugin} from "@vanilla-extract/webpack-plugin";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+import { VanillaExtractPlugin } from "@vanilla-extract/webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const webpackConfig = (env): Configuration => ({
-    entry: "./src/index.tsx",
-    ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        plugins: [new TsconfigPathsPlugin()]
-    },
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "build.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true
-                },
-                exclude: /dist/
+  entry: "./src/index.tsx",
+  ...(env.production || !env.development ? {} : { devtool: "eval-source-map" }),
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /dist/,
+      },
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              presets: [
+                "@babel/preset-typescript",
+                "@babel/preset-react",
+                ["@babel/preset-env", { targets: { node: 14 }, modules: false }],
+              ],
+              plugins: ["@vanilla-extract/babel-plugin"],
             },
-            {
-                test: /\.(js|ts|tsx)$/,
-                exclude: [/node_modules/],
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            babelrc: false,
-                            presets: [
-                                "@babel/preset-typescript",
-                                "@babel/preset-react",
-                                ["@babel/preset-env", {targets: {node: 14}, modules: false}]
-                            ],
-                            plugins: ["@vanilla-extract/babel-plugin"]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new ForkTsCheckerWebpackPlugin(),
-        new ESLintPlugin({files: "./src/**/*.{ts,tsx,js,jsx}"}),
-        new VanillaExtractPlugin(),
-        new MiniCssExtractPlugin()
-    ]
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new ESLintPlugin({ files: "./src/**/*.{ts,tsx,js,jsx}" }),
+    new VanillaExtractPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 });
 
 export default webpackConfig;
